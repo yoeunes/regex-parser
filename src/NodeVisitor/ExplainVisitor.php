38d0d4ae3a48296baec9a8aa2da688925c73c650
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-11-16 20:14:44 +0100

    WIP

---
+++ src/NodeVisitor/ExplainVisitor.php
@@ -0,0 +1,319 @@
+<?php
+
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RegexParser\NodeVisitor;
+
+use RegexParser\Node\AlternationNode;
+use RegexParser\Node\AnchorNode;
+use RegexParser\Node\AssertionNode;
+use RegexParser\Node\BackrefNode;
+use RegexParser\Node\CharClassNode;
+use RegexParser\Node\CharTypeNode;
+use RegexParser\Node\CommentNode;
+use RegexParser\Node\ConditionalNode;
+use RegexParser\Node\DotNode;
+use RegexParser\Node\GroupNode;
+use RegexParser\Node\GroupType;
+use RegexParser\Node\KeepNode;
+use RegexParser\Node\LiteralNode;
+use RegexParser\Node\OctalLegacyNode;
+use RegexParser\Node\OctalNode;
+use RegexParser\Node\PcreVerbNode;
+use RegexParser\Node\PosixClassNode;
+use RegexParser\Node\QuantifierNode;
+use RegexParser\Node\RangeNode;
+use RegexParser\Node\RegexNode;
+use RegexParser\Node\SequenceNode;
+use RegexParser\Node\SubroutineNode;
+use RegexParser\Node\UnicodeNode;
+use RegexParser\Node\UnicodePropNode;
+
+/**
+ * A visitor that explains the AST in a human-readable string.
+ *
+ * @implements NodeVisitorInterface<string>
+ */
+class ExplainVisitor implements NodeVisitorInterface
+{
+    /** @var array<string, string> */
+    private const CHAR_TYPE_MAP = [
+        'd' => 'any digit (0-9)',
+        'D' => 'any non-digit',
+        's' => 'any whitespace character',
+        'S' => 'any non-whitespace character',
+        'w' => 'any "word" character (alphanumeric or _)',
+        'W' => 'any "non-word" character',
+        'h' => 'any horizontal whitespace',
+        'H' => 'any non-horizontal whitespace',
+        'v' => 'any vertical whitespace',
+        'V' => 'any non-vertical whitespace',
+        'R' => 'a generic newline (\\r\\n, \\r, or \\n)',
+    ];
+
+    /** @var array<string, string> */
+    private const ANCHOR_MAP = [
+        '^' => 'the start of the string (or line, with /m flag)',
+        '$' => 'the end of the string (or line, with /m flag)',
+    ];
+
+    /** @var array<string, string> */
+    private const ASSERTION_MAP = [
+        'A' => 'the absolute start of the string',
+        'z' => 'the absolute end of the string',
+        'Z' => 'the end of the string (before final newline)',
+        'G' => 'the position of the last successful match',
+        'b' => 'a word boundary',
+        'B' => 'a non-word boundary',
+    ];
+
+    private int $indentLevel = 0;
+
+    public function visitRegex(RegexNode $node): string
+    {
+        $this->indentLevel = 0;
+        $patternExplain = $node->pattern->accept($this);
+        $flags = $node->flags ? ' (with flags: '.$node->flags.')' : '';
+
+        return sprintf("Regex matches%s:\n%s", $flags, $patternExplain);
+    }
+
+    public function visitAlternation(AlternationNode $node): string
+    {
+        $this->indentLevel++;
+        $alts = array_map(
+            fn (SequenceNode $alt) => $alt->accept($this),
+            $node->alternatives
+        );
+        $this->indentLevel--;
+
+        $indent = $this->indent();
+
+        return sprintf(
+            "EITHER:\n%s%s",
+            $indent,
+            implode(sprintf("\n%sOR:\n%s", $indent, $indent), $alts)
+        );
+    }
+
+    public function visitSequence(SequenceNode $node): string
+    {
+        $parts = array_map(fn ($child) => $child->accept($this), $node->children);
+
+        // Filter out empty strings (e.g., from empty nodes)
+        $parts = array_filter($parts, fn ($part) => '' !== $part);
+
+        return implode(sprintf("\n%s", $this->indent()), $parts);
+    }
+
+    public function visitGroup(GroupNode $node): string
+    {
+        $this->indentLevel++;
+        $childExplain = $node->child->accept($this);
+        $this->indentLevel--;
+
+        $indent = $this->indent();
+        $type = match ($node->type) {
+            GroupType::T_GROUP_CAPTURING => 'Start Capturing Group',
+            GroupType::T_GROUP_NON_CAPTURING => 'Start Non-Capturing Group',
+            GroupType::T_GROUP_NAMED => sprintf("Start Capturing Group (named: '%s')", $node->name),
+            GroupType::T_GROUP_LOOKAHEAD_POSITIVE => 'Start Positive Lookahead',
+            GroupType::T_GROUP_LOOKAHEAD_NEGATIVE => 'Start Negative Lookahead',
+            GroupType::T_GROUP_LOOKBEHIND_POSITIVE => 'Start Positive Lookbehind',
+            GroupType::T_GROUP_LOOKBEHIND_NEGATIVE => 'Start Negative Lookbehind',
+            GroupType::T_GROUP_ATOMIC => 'Start Atomic Group',
+            GroupType::T_GROUP_INLINE_FLAGS => sprintf("Start Group (with flags: '%s')", $node->flags),
+        };
+
+        return sprintf("%s:\n%s%s\n%sEnd Group", $type, $indent, $childExplain, $this->indent(false));
+    }
+
+    public function visitQuantifier(QuantifierNode $node): string
+    {
+        $childExplain = $node->node->accept($this);
+        $quantExplain = $this->explainQuantifierValue($node->quantifier, $node->type->value);
+
+        // If the child is simple (one line), put it on one line.
+        if (!str_contains($childExplain, "\n")) {
+            return sprintf('%s (%s)', $childExplain, $quantExplain);
+        }
+
+        // If the child is complex, indent it.
+        $this->indentLevel++;
+        $childExplain = $node->node->accept($this);
+        $this->indentLevel--;
+
+        return sprintf(
+            "Start Quantified Group (%s):\n%s%s\n%sEnd Quantified Group",
+            $quantExplain,
+            $this->indent(),
+            $childExplain,
+            $this->indent(false)
+        );
+    }
+
+    private function explainQuantifierValue(string $q, string $type): string
+    {
+        $desc = match ($q) {
+            '*' => 'zero or more times',
+            '+' => 'one or more times',
+            '?' => 'zero or one time',
+            default => preg_match('/^{(\d+)(?:,(\d*))?}$/', $q, $m) ?
+                (isset($m[2]) ? ('' === $m[2] ?
+                    sprintf('at least %d times', $m[1]) :
+                    sprintf('between %d and %d times', $m[1], $m[2])
+                ) :
+                    sprintf('exactly %d times', $m[1])
+                ) :
+                'with quantifier '.$q, // Fallback
+        };
+
+        $desc .= match ($type) {
+            'lazy' => ' (as few as possible)',
+            'possessive' => ' (and do not backtrack)',
+            default => '',
+        };
+
+        return $desc;
+    }
+
+    public function visitLiteral(LiteralNode $node): string
+    {
+        return 'Literal: '.($this->explainLiteral($node->value) ?? "''");
+    }
+
+    public function visitCharType(CharTypeNode $node): string
+    {
+        return 'Character Type: '.(self::CHAR_TYPE_MAP[$node->value] ?? 'unknown (\\'.$node->value.')');
+    }
+
+    public function visitDot(DotNode $node): string
+    {
+        return 'Wildcard: any character (except newline, unless /s flag is used)';
+    }
+
+    public function visitAnchor(AnchorNode $node): string
+    {
+        return 'Anchor: '.(self::ANCHOR_MAP[$node->value] ?? $node->value);
+    }
+
+    public function visitAssertion(AssertionNode $node): string
+    {
+        return 'Assertion: '.(self::ASSERTION_MAP[$node->value] ?? '\\'.$node->value);
+    }
+
+    public function visitKeep(KeepNode $node): string
+    {
+        return 'Assertion: \K (reset match start)';
+    }
+
+    public function visitCharClass(CharClassNode $node): string
+    {
+        $neg = $node->isNegated ? 'NOT ' : '';
+        $parts = array_map(fn ($part) => $part->accept($this), $node->parts);
+
+        return sprintf('Character Class: any character %sin [ %s ]', $neg, implode(', ', $parts));
+    }
+
+    public function visitRange(RangeNode $node): string
+    {
+        $start = $this->explainLiteral($node->start->value);
+        $end = $this->explainLiteral($node->end->value);
+
+        return sprintf('Range: from %s to %s', $start, $end);
+    }
+
+    public function visitBackref(BackrefNode $node): string
+    {
+        return sprintf('Backreference: matches text from group "%s"', $node->ref);
+    }
+
+    public function visitUnicode(UnicodeNode $node): string
+    {
+        return sprintf('Unicode: %s', $node->code);
+    }
+
+    public function visitUnicodeProp(UnicodePropNode $node): string
+    {
+        $type = str_starts_with($node->prop, '^') ? 'non-matching' : 'matching';
+        $prop = ltrim($node->prop, '^');
+
+        return sprintf('Unicode Property: any character %s "%s"', $type, $prop);
+    }
+
+    public function visitOctal(OctalNode $node): string
+    {
+        return 'Octal: '.$node->code;
+    }
+
+    public function visitOctalLegacy(OctalLegacyNode $node): string
+    {
+        return 'Legacy Octal: \\'.$node->code;
+    }
+
+    public function visitPosixClass(PosixClassNode $node): string
+    {
+        return 'POSIX Class: '.$node->class;
+    }
+
+    public function visitComment(CommentNode $node): string
+    {
+        return sprintf("Comment: '%s'", $node->comment);
+    }
+
+    public function visitConditional(ConditionalNode $node): string
+    {
+        $this->indentLevel++;
+        $cond = $node->condition->accept($this);
+        $yes = $node->yes->accept($this);
+        $no = $node->no->accept($this);
+        $this->indentLevel--;
+
+        $indent = $this->indent();
+
+        if ('' === $no) {
+            return sprintf("Conditional: IF (%s) THEN:\n%s%s", $cond, $indent, $yes);
+        }
+
+        return sprintf("Conditional: IF (%s) THEN:\n%s%s\n%sELSE:\n%s%s", $cond, $indent, $yes, $this->indent(false), $indent, $no);
+    }
+
+    public function visitSubroutine(SubroutineNode $node): string
+    {
+        $ref = match ($node->reference) {
+            'R' => 'the entire pattern',
+            '0' => 'the entire pattern',
+            default => 'group '.$node->reference,
+        };
+
+        return sprintf('Subroutine Call: recurses to %s', $ref);
+    }
+
+    public function visitPcreVerb(PcreVerbNode $node): string
+    {
+        return 'PCRE Verb: (*'.$node->verb.')';
+    }
+
+    private function indent(bool $withExtra = true): string
+    {
+        return str_repeat(' ', $this->indentLevel * 2).($withExtra ? '  ' : '');
+    }
+
+    private function explainLiteral(string $value): string
+    {
+        return match ($value) {
+            ' ' => "' ' (space)",
+            "\t" => "'\\t' (tab)",
+            "\n" => "'\\n' (newline)",
+            "\r" => "'\\r' (carriage return)",
+            default => ctype_print($value) ? "'".$value."'" : '(non-printable char)',
+        };
+    }
+}
---
+++ src/NodeVisitor/SampleGeneratorVisitor.php
@@ -0,0 +1,352 @@
+<?php
+
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RegexParser\NodeVisitor;
+
+use RegexParser\Node\AlternationNode;
+use RegexParser\Node\AnchorNode;
+use RegexParser\Node\AssertionNode;
+use RegexParser\Node\BackrefNode;
+use RegexParser\Node\CharClassNode;
+use RegexParser\Node\CharTypeNode;
+use RegexParser\Node\CommentNode;
+use RegexParser\Node\ConditionalNode;
+use RegexParser\Node\DotNode;
+use RegexParser\Node\GroupNode;
+use RegexParser\Node\GroupType;
+use RegexParser\Node\KeepNode;
+use RegexParser\Node\LiteralNode;
+use RegexParser\Node\NodeInterface;
+use RegexParser\Node\OctalLegacyNode;
+use RegexParser\Node\OctalNode;
+use RegexParser\Node\PcreVerbNode;
+use RegexParser\Node\PosixClassNode;
+use RegexParser\Node\QuantifierNode;
+use RegexParser\Node\RangeNode;
+use RegexParser\Node\RegexNode;
+use RegexParser\Node\SequenceNode;
+use RegexParser\Node\SubroutineNode;
+use RegexParser\Node\UnicodeNode;
+use RegexParser\Node\UnicodePropNode;
+
+/**
+ * A visitor that generates a random sample string that matches the AST.
+ *
+ * @implements NodeVisitorInterface<string>
+ */
+class SampleGeneratorVisitor implements NodeVisitorInterface
+{
+    /**
+     * @param int $maxRepetition Max times to repeat for * or + quantifiers
+     * to prevent excessively long or infinite samples.
+     */
+    public function __construct(private int $maxRepetition = 3)
+    {
+    }
+
+    /**
+     * Stores generated text from capturing groups.
+     * Keyed by both numeric index and name (if available).
+     * @var array<int|string, string>
+     */
+    private array $captures = [];
+    private int $groupCounter = 1;
+
+    public function visitRegex(RegexNode $node): string
+    {
+        // Reset state for this run
+        $this->captures = [];
+        $this->groupCounter = 1;
+
+        // Note: Flags (like /i) are ignored, as we generate the sample
+        // from the literal pattern.
+        return $node->pattern->accept($this);
+    }
+
+    public function visitAlternation(AlternationNode $node): string
+    {
+        // Pick one of the alternatives at random
+        $randomKey = array_rand($node->alternatives);
+        $chosenAlt = $node->alternatives[$randomKey];
+
+        return $chosenAlt->accept($this);
+    }
+
+    public function visitSequence(SequenceNode $node): string
+    {
+        $parts = array_map(fn ($child) => $child->accept($this), $node->children);
+
+        return implode('', $parts);
+    }
+
+    public function visitGroup(GroupNode $node): string
+    {
+        $result = $node->child->accept($this);
+
+        // Store the result if it's a capturing group
+        if (GroupType::T_GROUP_CAPTURING === $node->type) {
+            $this->captures[$this->groupCounter++] = $result;
+        } elseif (GroupType::T_GROUP_NAMED === $node->type) {
+            $this->captures[$this->groupCounter++] = $result;
+            if ($node->name) {
+                $this->captures[$node->name] = $result;
+            }
+        }
+
+        // For non-capturing, lookarounds, etc., just return the child's result
+        return $result;
+    }
+
+    public function visitQuantifier(QuantifierNode $node): string
+    {
+        [$min, $max] = $this->parseQuantifierRange($node->quantifier);
+
+        // Pick a random number of repetitions
+        $repeats = rand($min, $max);
+
+        $parts = [];
+        for ($i = 0; $i < $repeats; ++$i) {
+            $parts[] = $node->node->accept($this);
+        }
+
+        return implode('', $parts);
+    }
+
+    /**
+     * @return array{0: int, 1: int} [min, max]
+     */
+    private function parseQuantifierRange(string $q): array
+    {
+        return match ($q) {
+            '*' => [0, $this->maxRepetition],
+            '+' => [1, $this->maxRepetition],
+            '?' => [0, 1],
+            default => preg_match('/^{(\d+)(?:,(\d*))?}$/', $q, $m) ?
+                (isset($m[2]) ? ('' === $m[2] ?
+                    [$m[1], (int) $m[1] + $this->maxRepetition] : // {n,}
+                    [$m[1], (int) $m[2]] // {n,m}
+                ) :
+                    [$m[1], (int) $m[1]] // {n}
+                ) :
+                [0, 0], // Fallback (should be impossible)
+        };
+    }
+
+    public function visitLiteral(LiteralNode $node): string
+    {
+        return $node->value;
+    }
+
+    public function visitCharType(CharTypeNode $node): string
+    {
+        return $this->generateForCharType($node->value);
+    }
+
+    public function visitDot(DotNode $node): string
+    {
+        // Generate a random, simple, printable ASCII char
+        return $this->getRandomChar(['a', 'b', 'c', '1', '2', '3', ' ']);
+    }
+
+    public function visitAnchor(AnchorNode $node): string
+    {
+        // Anchors do not generate text
+        return '';
+    }
+
+    public function visitAssertion(AssertionNode $node): string
+    {
+        // Assertions do not generate text
+        return '';
+    }
+
+    public function visitKeep(KeepNode $node): string
+    {
+        // \K does not generate text
+        return '';
+    }
+
+    public function visitCharClass(CharClassNode $node): string
+    {
+        if ($node->isNegated) {
+            // Generating for a negated class is complex.
+            // We'd have to know the full set of all possible chars
+            // (ASCII? Unicode?) and subtract the parts.
+            // For a sample, it's safer to return a known "safe" char
+            // that is unlikely to be in the negated set.
+            return '!'; // e.g., a "safe" punctuation mark
+        }
+
+        if (empty($node->parts)) {
+            // e.g., [] which can never match
+            throw new \RuntimeException('Cannot generate sample for empty character class');
+        }
+
+        // Pick one of the parts at random
+        $randomKey = array_rand($node->parts);
+
+        return $node->parts[$randomKey]->accept($this);
+    }
+
+    public function visitRange(RangeNode $node): string
+    {
+        if (!$node->start instanceof LiteralNode || !$node->end instanceof LiteralNode) {
+            // Should be caught by Validator, but good to check
+            return $node->start->accept($this);
+        }
+
+        // Generate a random character within the ASCII range
+        try {
+            return chr(rand(ord($node->start->value), ord($node->end->value)));
+        } catch (\Throwable) {
+            // Fallback if ord() fails or range is invalid
+            return $node->start->value;
+        }
+    }
+
+    public function visitBackref(BackrefNode $node): string
+    {
+        $ref = $node->ref;
+        if (ctype_digit($ref)) {
+            $ref = (int) $ref;
+        }
+
+        if (isset($this->captures[$ref])) {
+            return $this->captures[$ref];
+        }
+
+        // Handle named \k<name> or \k{name}
+        if (preg_match('/^k<([a-zA-Z0-9_]+)>$/', $ref, $m) || preg_match('/^k{([a-zA-Z0-9_]+)}$/', $ref, $m)) {
+            return $this->captures[$m[1]] ?? '';
+        }
+
+        // Backreference to a group that hasn't matched yet
+        // (or doesn't exist). In a real engine, this fails the match.
+        // For generation, we must return empty string.
+        return '';
+    }
+
+    public function visitUnicode(UnicodeNode $node): string
+    {
+        if (preg_match('/^\\\\x([0-9a-fA-F]{2})$/', $node->code, $m)) {
+            return chr(hexdec($m[1]));
+        }
+        if (preg_match('/^\\\\u\{([0-9a-fA-F]+)\}$/', $node->code, $m)) {
+            return mb_chr(hexdec($m[1]), 'UTF-8');
+        }
+        // Fallback for unknown unicode
+        return '?';
+    }
+
+    public function visitUnicodeProp(UnicodePropNode $node): string
+    {
+        // Too complex to generate a *random* char for a property.
+        // Return a known-good sample.
+        if (str_contains($node->prop, 'L')) { // 'L' (Letter)
+            return $this->getRandomChar(['a', 'b', 'c']);
+        }
+        if (str_contains($node->prop, 'N')) { // 'N' (Number)
+            return $this->getRandomChar(['1', '2', '3']);
+        }
+        if (str_contains($node->prop, 'P')) { // 'P' (Punctuation)
+            return $this->getRandomChar(['.', ',', '!']);
+        }
+
+        return $this->getRandomChar(['a', '1', '.']); // Generic fallback
+    }
+
+    public function visitOctal(OctalNode $node): string
+    {
+        if (preg_match('/^\\\\o\{([0-7]+)\}$/', $node->code, $m)) {
+            return mb_chr(octdec($m[1]), 'UTF-8');
+        }
+
+        return '?';
+    }
+
+    public function visitOctalLegacy(OctalLegacyNode $node): string
+    {
+        return mb_chr(octdec($node->code), 'UTF-8');
+    }
+
+    public function visitPosixClass(PosixClassNode $node): string
+    {
+        return match (strtolower($node->class)) {
+            'alpha' => $this->getRandomChar(['a', 'b', 'C', 'Z']),
+            'alnum' => $this->getRandomChar(['a', 'Z', '1', '9']),
+            'digit' => $this->getRandomChar(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']),
+            'xdigit' => $this->getRandomChar(['0', '9', 'a', 'f', 'A', 'F']),
+            'space' => $this->getRandomChar([' ', "\t", "\n"]),
+            'lower' => $this->getRandomChar(['a', 'b', 'c', 'z']),
+            'upper' => $this->getRandomChar(['A', 'B', 'C', 'Z']),
+            'punct' => $this->getRandomChar(['.', '!', ',', '?']),
+            default => $this->getRandomChar(['a', '1', ' ']),
+        };
+    }
+
+    public function visitComment(CommentNode $node): string
+    {
+        // Comments do not generate text
+        return '';
+    }
+
+    public function visitConditional(ConditionalNode $node): string
+    {
+        // This is complex. Does the condition (e.g., group 1) exist?
+        // We'll randomly choose to satisfy the condition or not.
+        if (rand(0, 1) === 1) {
+            // Simulate "YES" path
+            return $node->yes->accept($this);
+        }
+
+        // Simulate "NO" path
+        return $node->no->accept($this);
+    }
+
+    public function visitSubroutine(SubroutineNode $node): string
+    {
+        // Recursive generation is a deep computer science problem
+        // (can lead to infinite loops). Safest to throw.
+        throw new \LogicException('Sample generation for subroutines is not supported.');
+    }
+
+    public function visitPcreVerb(PcreVerbNode $node): string
+    {
+        // Verbs do not generate text
+        return '';
+    }
+
+    /**
+     * @param array<string> $chars
+     */
+    private function getRandomChar(array $chars): string
+    {
+        return $chars[array_rand($chars)];
+    }
+
+    private function generateForCharType(string $type): string
+    {
+        return match ($type) {
+            'd' => (string) rand(0, 9),
+            'D' => $this->getRandomChar(['a', ' ', '!']), // Not a digit
+            's' => $this->getRandomChar([' ', "\t", "\n"]),
+            'S' => $this->getRandomChar(['a', '1', '!']), // Not whitespace
+            'w' => $this->getRandomChar(['a', 'Z', '5', '_']),
+            'W' => $this->getRandomChar(['!', ' ', '@']), // Not word
+            'h' => $this->getRandomChar([' ', "\t"]),
+            'H' => $this->getRandomChar(['a', '1', "\n"]), // Not horiz space
+            'v' => "\n", // vertical space
+            'V' => $this->getRandomChar(['a', '1', ' ']), // Not vert space
+            'R' => $this->getRandomChar(["\r\n", "\r", "\n"]),
+            default => '?',
+        };
+    }
+}
