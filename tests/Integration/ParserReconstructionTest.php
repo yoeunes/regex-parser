efe252f15923d8ee1b29f2cd3f1fd40856a8a283
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-11-19 01:53:21 +0100

    Adds license headers to test files
    Adds license headers to the integration test files for consistency and to comply with licensing requirements.

--- tests/Integration/ParserReconstructionTest.php
+++ tests/Integration/ParserReconstructionTest.php
@@ -2,11 +2,20 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace RegexParser\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
-use RegexParser\Parser;
 use RegexParser\Node\CommentNode;
+use RegexParser\Parser;
 
 class ParserReconstructionTest extends TestCase
 {
