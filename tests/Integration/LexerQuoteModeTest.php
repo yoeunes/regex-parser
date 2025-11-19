ae5ba63c71fbe27076ac3915ef71cfb5fbf36eae
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-11-19 01:22:31 +0100

    Adds missing license headers
    Adds missing license headers and declare strict types
    to integration tests files.

--- tests/Integration/LexerQuoteModeTest.php
+++ tests/Integration/LexerQuoteModeTest.php
@@ -1,10 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace RegexParser\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 use RegexParser\Lexer;
-use RegexParser\TokenType;
 
 class LexerQuoteModeTest extends TestCase
 {
--- tests/Integration/VisitorFallbackTest.php
+++ tests/Integration/VisitorFallbackTest.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace RegexParser\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
@@ -68,7 +79,7 @@ class VisitorFallbackTest extends TestCase
         // We force it by mocking a NodeInterface that returns a DIFFERENT instance when visited.
 
         $mockPart = $this->createMock(NodeInterface::class);
-        $mockPart->expects($this->any())
+        $mockPart
             ->method('accept')
             ->willReturn(new LiteralNode('changed', 0, 0)); // Return different instance
 
