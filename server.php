2dbc6a409a433f2160735f2b841938cf4f16b86b
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-11-29 19:41:21 +0100

    Updates server start message for clarity
    Updates the server start message to include the "http://"
    prefix for the address. This makes it clearer to the user
    that they should use a web browser to access the server.
    
    Also adds a message to indicate how to stop the server.

--- server.php
+++ server.php
@@ -15,9 +15,10 @@ $host = '0.0.0.0';
 $port = 5000;
 $docRoot = __DIR__.'/public';
 
-echo "Starting PHP development server on {$host}:{$port}\n";
+echo "Starting PHP development server on http://{$host}:{$port}\n";
 echo "Document root: {$docRoot}\n";
 echo "Server is ready!\n\n";
+echo "Press Ctrl+C to stop the server.\n";
 
 chdir($docRoot);
 
