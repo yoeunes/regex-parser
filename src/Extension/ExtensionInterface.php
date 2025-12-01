8110cb29e3c1688e94ff4654489b3fd75c8063c7
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-12-01 02:12:28 +0100

    Adds extension interface and base class
    Provides a foundational structure for integrating custom logic into the RegexParser.
    
    Introduces `ExtensionInterface` to define a contract for extensions, ensuring they provide a method for retrieving node visitors. `AbstractExtension` offers a convenient abstract base class for developers, providing a default empty implementation for extensions that may not have custom node visitors.

---
+++ src/Extension/ExtensionInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RegexParser\Extension;
+
+use RegexParser\NodeVisitor\NodeVisitorInterface;
+
+interface ExtensionInterface
+{
+    /**
+     * @return array<NodeVisitorInterface>
+     */
+    public function getNodeVisitors(): array;
+}
