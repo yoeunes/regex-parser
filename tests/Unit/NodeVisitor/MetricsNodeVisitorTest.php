ea2074d9ff95394098b352be85954fc9d07331da
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-12-04 06:31:41 +0100

    Refactors metrics node visitor test
    Updates the test method name to adhere to snake_case convention.
    Adds the standard copyright header for file consistency.

--- tests/Unit/NodeVisitor/MetricsNodeVisitorTest.php
+++ tests/Unit/NodeVisitor/MetricsNodeVisitorTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace RegexParser\Tests\Unit\NodeVisitor;
 
 use PHPUnit\Framework\TestCase;
@@ -10,14 +19,14 @@ use RegexParser\Regex;
 
 final class MetricsNodeVisitorTest extends TestCase
 {
-    public function testItCollectsCountsAndDepth(): void
+    public function test_it_collects_counts_and_depth(): void
     {
         $ast = Regex::create()->parse('/(a|b)c/');
         $metrics = $ast->accept(new MetricsNodeVisitor());
 
-        self::assertSame(7, $metrics['total']);
-        self::assertSame(3, $metrics['counts']['LiteralNode'] ?? null);
-        self::assertSame(1, $metrics['counts']['AlternationNode'] ?? null);
-        self::assertGreaterThanOrEqual(4, $metrics['maxDepth']);
+        $this->assertSame(7, $metrics['total']);
+        $this->assertSame(3, $metrics['counts']['LiteralNode'] ?? null);
+        $this->assertSame(1, $metrics['counts']['AlternationNode'] ?? null);
+        $this->assertGreaterThanOrEqual(4, $metrics['maxDepth']);
     }
 }
