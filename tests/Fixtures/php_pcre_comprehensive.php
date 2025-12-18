65f9b9afea82d8269424c8e20763bae050146125
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-12-18 14:44:30 +0100

    Cleans up test fixture files

--- tests/Fixtures/php_pcre_comprehensive.php
+++ tests/Fixtures/php_pcre_comprehensive.php
@@ -1,16 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of the RegexParser package.
- *
- * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 /*
  * Comprehensive PCRE Test Suite extracted from php-src/ext/pcre/tests
  * Contains 216 test cases covering preg_* functions and edge cases
