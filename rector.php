bc84601673428b5af48276ec19366c15bd97e659
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-12-18 14:51:47 +0100

    Fixes removes empty config path
    The change removes the previously included 'config' path from the $rectorConfig->paths array to maintain clean configuration file management.
    
      Removes unused config path in rector configuration

--- rector.php
+++ rector.php
@@ -24,7 +24,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__.'/src',
         __DIR__.'/tests',
-        __DIR__.'/config',
         __DIR__.'/bin',
     ]);
 
