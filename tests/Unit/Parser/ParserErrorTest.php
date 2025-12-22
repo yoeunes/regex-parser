67647eb22cf48b3299ddcc9e6216470bbc8e89a1
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-12-22 18:06:27 +0100

    feat: complete PHPUnit test reorganization - eliminate legacy directories
    - Move remaining legacy lexer tests from tests/Lexer/ to tests/Unit/Lexer/
    - Move remaining legacy parser tests from tests/Parser/ to tests/Unit/Parser/
    - Move remaining legacy ReDoS tests from tests/ReDoS/ to tests/Unit/ReDoS/
    - Move regression tests to tests/Functional/Regression/
    - Update all namespaces to match new directory structure
    - Remove empty legacy directories (Lexer, Parser, ReDoS, Regression)
    - Keep complementary quote mode tests as they test different scenarios
    
    Result: Clean, professional test structure with no legacy directories,
    following Symfony best practices for test organization.

--- tests/Unit/Parser/ParserErrorTest.php
+++ tests/Unit/Parser/ParserErrorTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace RegexParser\Tests\Parser;
+namespace RegexParser\Tests\Unit\Parser;
 
 use PHPUnit\Framework\TestCase;
 use RegexParser\Exception\ParserException;
--- tests/Unit/ReDoS/ReDoSAnalyzerTest.php
+++ tests/Unit/ReDoS/ReDoSAnalyzerTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace RegexParser\Tests\ReDoS;
+namespace RegexParser\Tests\Unit\ReDoS;
 
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
