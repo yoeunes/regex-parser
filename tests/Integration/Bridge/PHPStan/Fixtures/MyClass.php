69621465ea8c471e1e2e73d4a5c4bb8b4fe559b0
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-12-22 19:33:13 +0100

    Moves test namespaces to reflect directory structure

--- tests/Integration/Bridge/PHPStan/Fixtures/MyClass.php
+++ tests/Integration/Bridge/PHPStan/Fixtures/MyClass.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace RegexParser\Tests\Bridge\PHPStan\Fixtures;
+namespace RegexParser\Tests\Integration\Bridge\PHPStan\Fixtures;
 
 final class MyClass
 {
