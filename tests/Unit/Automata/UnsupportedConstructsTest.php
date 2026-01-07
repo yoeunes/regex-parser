591ce5b8a098edf1181220ee26da09908daf3396
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2026-01-07 02:50:39 +0100

    Add automata solver test scaffolding

---
+++ tests/Unit/Automata/UnsupportedConstructsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the RegexParser package.
+ *
+ * (c) Younes ENNAJI <younes.ennaji.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RegexParser\Tests\Unit\Automata;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use RegexParser\Automata\RegexSolver;
+use RegexParser\Exception\ComplexityException;
+
+final class UnsupportedConstructsTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('provideNonRegularPatterns')]
+    public function test_non_regular_patterns_throw_complexity_exception(string $pattern): void
+    {
+        $solver = new RegexSolver();
+
+        $this->expectException(ComplexityException::class);
+
+        $solver->intersection($pattern, '/a/');
+    }
+
+    public static function provideNonRegularPatterns(): \Generator
+    {
+        yield 'lookahead' => ['/(?=a)a/'];
+        yield 'lookbehind' => ['/(?<=a)b/'];
+        yield 'backreference' => ['/(a)\\1/'];
+        yield 'recursion' => ['/(?R)/'];
+        yield 'subroutine' => ['/(a)(?1)/'];
+    }
+}
