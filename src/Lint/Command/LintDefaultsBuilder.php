77e91b521df930d7465d4624a8d0661aef7ff8c4
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2026-01-22 19:24:48 +0100

    feat: add new 'checks' configuration format with BC layer for 'rules'
    Introduces a new 'checks' configuration format that provides a more structured
    and extensible way to configure linting checks. The old 'rules' format remains
    supported for backwards compatibility.
    
    Changes:
    - Add support for 'checks' configuration (validation, redos, optimizations)
    - Support both boolean and object forms for redos/optimizations checks
    - Add new options: redos.mode, redos.threshold, redos.noJit, optimizations.minSavings
    - Maintain backwards compatibility with deprecated 'rules' format
    - Deprecated 'rules' now takes precedence over 'checks' if both set
    
    Related to corpus updates filtering unconfirmed ReDoS findings.

--- src/Lint/Command/LintDefaultsBuilder.php
+++ src/Lint/Command/LintDefaultsBuilder.php
@@ -44,14 +44,61 @@ final class LintDefaultsBuilder
             $defaults['format'] = $config['format'];
         }
 
+        // Handle new "checks" configuration format (preferred)
+        if (isset($config['checks']) && \is_array($config['checks'])) {
+            if (\array_key_exists('validation', $config['checks'])) {
+                $defaults['checkValidation'] = $config['checks']['validation'];
+            }
+
+            // Handle checks.redos (can be boolean or object)
+            if (\array_key_exists('redos', $config['checks'])) {
+                $redos = $config['checks']['redos'];
+                if (\is_bool($redos)) {
+                    $defaults['checkRedos'] = $redos;
+                } elseif (\is_array($redos)) {
+                    if (\array_key_exists('enabled', $redos)) {
+                        $defaults['checkRedos'] = $redos['enabled'];
+                    }
+                    if (\array_key_exists('mode', $redos)) {
+                        $defaults['redosMode'] = $redos['mode'];
+                    }
+                    if (\array_key_exists('threshold', $redos)) {
+                        $defaults['redosThreshold'] = $redos['threshold'];
+                    }
+                    if (\array_key_exists('noJit', $redos)) {
+                        $defaults['redosNoJit'] = $redos['noJit'];
+                    }
+                }
+            }
+
+            // Handle checks.optimizations (can be boolean or object)
+            if (\array_key_exists('optimizations', $config['checks'])) {
+                $optimizations = $config['checks']['optimizations'];
+                if (\is_bool($optimizations)) {
+                    $defaults['checkOptimizations'] = $optimizations;
+                } elseif (\is_array($optimizations)) {
+                    if (\array_key_exists('enabled', $optimizations)) {
+                        $defaults['checkOptimizations'] = $optimizations['enabled'];
+                    }
+                    if (\array_key_exists('minSavings', $optimizations)) {
+                        $defaults['minSavings'] = $optimizations['minSavings'];
+                    }
+                    if (\array_key_exists('options', $optimizations) && \is_array($optimizations['options'])) {
+                        $defaults['optimizations'] = $optimizations['options'];
+                    }
+                }
+            }
+        }
+
+        // Handle deprecated "rules" configuration format (for BC)
         if (isset($config['rules']) && \is_array($config['rules'])) {
-            if (\array_key_exists('redos', $config['rules'])) {
+            if (\array_key_exists('redos', $config['rules']) && !isset($defaults['checkRedos'])) {
                 $defaults['checkRedos'] = $config['rules']['redos'];
             }
-            if (\array_key_exists('validation', $config['rules'])) {
+            if (\array_key_exists('validation', $config['rules']) && !isset($defaults['checkValidation'])) {
                 $defaults['checkValidation'] = $config['rules']['validation'];
             }
-            if (\array_key_exists('optimization', $config['rules'])) {
+            if (\array_key_exists('optimization', $config['rules']) && !isset($defaults['checkOptimizations'])) {
                 $defaults['checkOptimizations'] = $config['rules']['optimization'];
             }
         }
@@ -60,19 +107,20 @@ final class LintDefaultsBuilder
             $defaults['ide'] = $config['ide'];
         }
 
-        if (isset($config['redosMode'])) {
+        // Handle deprecated top-level redos options (for BC, only if not set by checks)
+        if (isset($config['redosMode']) && !isset($defaults['redosMode'])) {
             $defaults['redosMode'] = $config['redosMode'];
         }
 
-        if (isset($config['redosThreshold'])) {
+        if (isset($config['redosThreshold']) && !isset($defaults['redosThreshold'])) {
             $defaults['redosThreshold'] = $config['redosThreshold'];
         }
 
-        if (isset($config['redosNoJit'])) {
+        if (isset($config['redosNoJit']) && !isset($defaults['redosNoJit'])) {
             $defaults['redosNoJit'] = $config['redosNoJit'];
         }
 
-        if (isset($config['optimizations'])) {
+        if (isset($config['optimizations']) && !isset($defaults['optimizations'])) {
             $defaults['optimizations'] = $config['optimizations'];
         }
 
